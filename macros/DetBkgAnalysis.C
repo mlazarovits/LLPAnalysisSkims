5c24830853d7ed4f9766569481767512af00e9b1
Margaret Lazarovits <mlazarovits@gmail.com>
2025-11-03 11:32:22 -0600

    added hists for pred and true CNN maps

--- macros/DetBkgAnalysis.C
+++ macros/DetBkgAnalysis.C
@@ -10,23 +10,71 @@ void DetBkgAnalysis(){
 		return;
 	}
 
+	//rh maps	
 	TTreeInterface TI(file,"tree");
-
-
 	vector<string> branchlist;
 	branchlist.push_back("SC_trueLabel");
-	branchlist.push_back("SC_predLabel");
+	branchlist.push_back("SC_predScore_physBkg");
+	branchlist.push_back("SC_predScore_BH");
+	branchlist.push_back("SC_predScore_spike");
+	//branchlist.push_back("SC_EtaVar");
+	//branchlist.push_back("SC_PhiVar");
+	//branchlist.push_back("SC_TimeVar");
+	//branchlist.push_back("SC_Energy");
 
 	vector<string> subbranchlist;
-	TI.SetNSubBranch("SC_nRHs");
+	TI.SetNSubBranch("SC_nRHs_grid");
 	subbranchlist.push_back("rh_iEta");
 	subbranchlist.push_back("rh_iPhi");
 	subbranchlist.push_back("rh_energy");
 
 	string csvname = "SCsUnrolled.csv";
-	TI.CreateFlattenedCSV(branchlist, subbranchlist, csvname);
+	if(gSystem->AccessPathName(file.c_str())){
+		TI.CreateFlattenedCSV(branchlist, subbranchlist, csvname);
+	}
 	
-	//ROOT::RDataFrame df = ROOT::RDF::FromCSV(csvname.c_str(),true,' ',-1LL,std::move(coltypes));
+	ROOT::RDataFrame df = ROOT::RDF::FromCSV(csvname.c_str(),true,' ');
 
+	vector<ROOT::RDF::RResultPtr<TH1D>> hists1d;
+	vector<ROOT::RDF::RResultPtr<TH2D>> hists2d;
+	auto truelab1_map = df.Filter("SC_trueLabel == 1").Histo2D({"physBkgMap_true","physBkgMap_true;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(truelab1_map);
+	auto truelab2_map = df.Filter("SC_trueLabel == 2").Histo2D({"beamHaloMap_true","beamHaloMap_true;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(truelab2_map);
+	auto truelab3_map = df.Filter("SC_trueLabel == 3").Histo2D({"spikeMap_true","spikeMap_true;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(truelab3_map);
+
+
+	string physbkg_thresh = "0.67";	
+	auto predlab1_map = df.Filter("SC_predScore_physBkg > "+physbkg_thresh).Histo2D({"physBkgMap_pred","physBkgMap_pred;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(predlab1_map);
+	
+	string bh_thresh = "0.87";	
+	auto predlab2_map = df.Filter("SC_predScore_BH > "+bh_thresh).Histo2D({"beamHaloMap_pred","beamHaloMap_pred;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(predlab2_map);
+	
+	string spike_thresh = "0.65";	
+	auto predlab3_map = df.Filter("SC_predScore_spike > "+spike_thresh).Histo2D({"spikeMap_pred","spikeMap_pred;rh_ieta;rh_iphi;energy",7,-3,4,7,-3,4}, "rh_iEta","rh_iPhi","rh_energy");
+	hists2d.push_back(predlab3_map);
+
+
+
+	//do PU subclusters
+	//TI = TTreeInterface(file,"tree");
+	//branchlist.clear();
+	//branchlist.push_back("SC_EtaVar");
+	//branchlist.push_back("SC_PhiVar");
+	//branchlist.push_back("SC_TimeVar");
+	//branchlist.push_back("SC_Energy");
+
+
+
+	string ofilename = "test.root";
+	cout << "Writing output to " << ofilename << endl;
+	TFile* ofile = new TFile(ofilename.c_str(),"RECREATE");
+	ofile->cd();
+	for(auto hist : hists1d) hist->Write();
+	for(auto hist : hists2d) hist->Write();
+	ofile->Close();
 
 }
