3d9b492158b5fd3f8d74f424f5a6ba093139684b
Margaret Lazarovits <mlazarovits@gmail.com>
2025-11-17 10:05:43 -0600

    ignoring root and csv files, updated rootlogon

---
+++ rootlogon.C
@@ -0,0 +1,8 @@
+{
+//gROOT->ProcessLine(" #include "\
+//gSystem->Load("lib/libvecDict.so");
+//gROOT->ProcessLine(".x SetPalette.C()");
+gStyle->SetPadGridX(true);
+gStyle->SetPadGridY(true);
+gStyle->SetOptStat(0000);
+}
