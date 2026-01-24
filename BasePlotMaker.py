d3455cfcb3204651be683f1a7662a5202136138e
Margaret Lazarovits <mlazarovits@gmail.com>
2026-01-24 10:42:37 -0600

    renamed UnrollMaker to BasePlotMaker, now includes ComparisonMaker

--- BasePlotMaker.py
+++ BasePlotMaker.py
@@ -1,6 +1,54 @@
 import ROOT
 from typing import Dict, List, Tuple, Optional
 import numpy as np
+import cmsstyle as CMS
+class ComparisonMaker:
+    def __init__(self):
+        self._ensure_mc_colors()
+
+
+    def _ensure_mc_colors(self):
+        """Force recreation of MC colors at specific indices to override palette interference."""
+        # Define the hex colors and expected indices from rootlogon.C
+        hex_colors = ["#5A4484", "#347889", "#F4B240", "#E54B26", "#C05780", "#7A68A6", "#2E8B57", "#8B4513"]
+        expected_indices = [1179, 1180, 1181, 1182, 1183, 1184, 1185, 1186]
+        
+        for i, hex_color in enumerate(hex_colors):
+            expected_index = expected_indices[i]
+            
+            # Parse hex color to RGB
+            hex_clean = hex_color.replace("#", "")
+            r = int(hex_clean[0:2], 16) / 255.0
+            g = int(hex_clean[2:4], 16) / 255.0  
+            b = int(hex_clean[4:6], 16) / 255.0
+            
+            # Get existing color or create new one
+            existing_color = ROOT.gROOT.GetColor(expected_index)
+            if existing_color:
+                # Update existing color's RGB values
+                existing_color.SetRGB(r, g, b)
+            else:
+                # Create new color at specific index
+                ROOT.TColor(expected_index, r, g, b)
+    
+    def _setup_comparison_canvas(self, canvas_name, x_min, x_max, x_label="", canvas_width=1100, canvas_height=800):
+        """Shared canvas setup for data/MC comparison plots."""
+        canvas = CMS.cmsCanvas(canvas_name, x_min, x_max, 0, 1, x_label, "Events", square=False, extraSpace=0.01, iPos=0)
+        canvas.SetCanvasSize(canvas_width, canvas_height)
+        
+        # Create pads for main plot and ratio
+        pad1 = ROOT.TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
+        pad2 = ROOT.TPad("pad2", "pad2", 0, 0.0, 1, 0.3)
+        
+        pad1.SetBottomMargin(0.01)
+        pad1.SetTopMargin(0.1)
+        pad2.SetBottomMargin(0.4)
+        
+        canvas.cd()
+        pad1.Draw()
+        pad2.Draw()
+        
+        return canvas, pad1, pad2
 
 class UnrollMaker:
     def __init__(self):
