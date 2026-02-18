db391b32dc26dab014eb741e54846e5ff54c94c7
Margaret Lazarovits <mlazarovits@gmail.com>
2026-02-18 12:27:52 -0600

    added arg for plot format

--- draw_canvases.py
+++ draw_canvases.py
@@ -4,10 +4,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--input","-i",help="file with canvases to draw",required=True)
 parser.add_argument("--odir","-o",help="output directory for plots",required=True)
+parser.add_argument("--format","-f",help="format to save plots",default="png")
+args = parser.parse_args()
 
 file = ROOT.TFile.Open(args.input,"READ")
 for key in file.GetListOfKeys():
 	can = file.Get(key.GetName())
 	can.Draw()
-	can.SaveAs(args.odir+"/"+can.GetName()+".png")
+	can.SaveAs(args.odir+"/"+can.GetName()+"."+args.format)
 file.Close()
